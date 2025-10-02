dd76d06f4ba8f87b39befad39414e0a4587fb385
PEDRO HENRIQUE RIGUETE DE OLIVEIRA MARTINS <154231408+RIGUETE-PEDRO@users.noreply.github.com>
2025-10-01 23:53:27 -0300

    dml.sql

---
+++ dml.sql
@@ -0,0 +1,5 @@
+INSERT INTO TipoUsuario(TipoUsuario) VALUES ('User');
+INSERT INTO TipoUsuario(TipoUsuario) VALUES ('Admin');
+
+INSERT INTO Tarefas(Tarefa) VALUES ('Cadastrar Usuario');
+INSERT INTO Tarefas(Tarefa) VALUES ('login');
