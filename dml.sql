07912d7c8f5aecd24c139f121c18d9eb9b4284b7
PEDRO HENRIQUE RIGUETE DE OLIVEIRA MARTINS <154231408+RIGUETE-PEDRO@users.noreply.github.com>
2025-10-13 23:31:41 -0300

    adicionando paises
    Inserted multiple country names into the Pais table.

--- dml.sql
+++ dml.sql
@@ -3,3 +3,19 @@ INSERT INTO TipoUsuario(TipoUsuario) VALUES ('Admin');
 
 INSERT INTO Tarefas(Tarefa) VALUES ('Cadastrar Usuario');
 INSERT INTO Tarefas(Tarefa) VALUES ('login');
+
+
+--insert de paises 
+
+INSERT INTO Pais (Nome)
+VALUES 
+('Afeganistão'), ('África do Sul'), ('Albânia'), ('Alemanha'), ('Andorra'),
+('Angola'), ('Antígua e Barbuda'), ('Arábia Saudita'), ('Argélia'), ('Argentina'),
+('Armênia'), ('Austrália'), ('Áustria'), ('Azerbaijão'), ('Bahamas'),
+('Bahrein'), ('Bangladesh'), ('Barbados'), ('Bélgica'), ('Belize'),
+('Benin'), ('Bielorrússia'), ('Bolívia'), ('Bósnia e Herzegovina'), ('Botsuana'),
+('Brasil'), ('Brunei'), ('Bulgária'), ('Burkina Faso'), ('Burundi'),
+('Cabo Verde'), ('Camarões'), ('Camboja'), ('Canadá'), ('Catar'),
+('Cazaquistão'), ('Chade'), ('Chile'), ('China'), ('Chipre'),
+('Colômbia'), ('Comores'), ('Coreia do Norte'), ('Coreia do Sul'), ('Costa do Marfim'),
+('Costa Rica'), ('Croácia'), ('Cuba'), ('Dinamarca'), ('Djibuti');
