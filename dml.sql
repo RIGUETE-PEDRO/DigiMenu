fe219e2ced7f731b6a7372d85a8729d51b88bf6c
PEDRO HENRIQUE RIGUETE DE OLIVEIRA MARTINS <154231408+RIGUETE-PEDRO@users.noreply.github.com>
2025-10-17 00:37:45 -0300

    Add multiple tasks and status entries in dml.sql

--- dml.sql
+++ dml.sql
@@ -3,3 +3,32 @@ INSERT INTO TipoUsuario(TipoUsuario) VALUES ('Admin');
 
 INSERT INTO Tarefas(Tarefa) VALUES ('Cadastrar Usuario');
 INSERT INTO Tarefas(Tarefa) VALUES ('login');
+insert into Tarefas (Tarefa) values ('Cadastro de Produto');
+insert into Tarefas (Tarefa) values ('Atualização de Produto');
+insert into Tarefas (Tarefa) values ('Delete de produto ');
+insert into Tarefas (Tarefa) values ('adicionar produto no carrinho');
+
+
+--status de pedido
+insert into status values ('pendente')
+insert into status values ('em preparo')
+insert into status values ('pronto')
+insert into status values ('saiu para entrega')
+insert into status values ('finalizado')
+
+
+
+--insert de paises 
+
+INSERT INTO Pais (Nome)
+VALUES 
+('Afeganistão'), ('África do Sul'), ('Albânia'), ('Alemanha'), ('Andorra'),
+('Angola'), ('Antígua e Barbuda'), ('Arábia Saudita'), ('Argélia'), ('Argentina'),
+('Armênia'), ('Austrália'), ('Áustria'), ('Azerbaijão'), ('Bahamas'),
+('Bahrein'), ('Bangladesh'), ('Barbados'), ('Bélgica'), ('Belize'),
+('Benin'), ('Bielorrússia'), ('Bolívia'), ('Bósnia e Herzegovina'), ('Botsuana'),
+('Brasil'), ('Brunei'), ('Bulgária'), ('Burkina Faso'), ('Burundi'),
+('Cabo Verde'), ('Camarões'), ('Camboja'), ('Canadá'), ('Catar'),
+('Cazaquistão'), ('Chade'), ('Chile'), ('China'), ('Chipre'),
+('Colômbia'), ('Comores'), ('Coreia do Norte'), ('Coreia do Sul'), ('Costa do Marfim'),
+('Costa Rica'), ('Croácia'), ('Cuba'), ('Dinamarca'), ('Djibuti');
