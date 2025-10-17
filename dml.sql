41344379c2e6b06dd81ded977d619f3acc6d1656
PEDRO HENRIQUE RIGUETE DE OLIVEIRA MARTINS <154231408+RIGUETE-PEDRO@users.noreply.github.com>
2025-10-16 23:43:50 -0300

    Insert new order statuses into the database
    Added new status entries for order processing.

--- dml.sql
+++ dml.sql
@@ -6,3 +6,11 @@ INSERT INTO Tarefas(Tarefa) VALUES ('login');
 insert into Tarefas (Tarefa) values ('Cadastro de Produto');
 insert into Tarefas (Tarefa) values ('Atualização de Produto');
 insert into Tarefas (Tarefa) values ('Delete de produto ');
+
+
+--status de pedido
+insert into status values ('pendente')
+insert into status values ('em preparo')
+insert into status values ('pronto')
+insert into status values ('saiu para entrega')
+insert into status values ('finalizado')
