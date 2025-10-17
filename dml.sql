75e29d268db4df177196cfecd3e6dec9e0d1694b
PEDRO HENRIQUE RIGUETE DE OLIVEIRA MARTINS <154231408+RIGUETE-PEDRO@users.noreply.github.com>
2025-10-17 00:37:33 -0300

    Add countries to Pais table
    Inserted multiple countries into the Pais table.

--- dml.sql
+++ dml.sql
@@ -15,3 +15,20 @@ insert into status values ('em preparo')
 insert into status values ('pronto')
 insert into status values ('saiu para entrega')
 insert into status values ('finalizado')
+
+
+
+--insert de paises 
+
+INSERT INTO Pais (Nome)
+VALUES 
+('Afeganistão'), ('África do Sul'), ('Albânia'), ('Alemanha'), ('Andorra'),
+('Angola'), ('Antígua e Barbuda'), ('Arábia Saudita'), ('Argélia'), ('Argentina'),
+('Armênia'), ('Austrália'), ('Áustria'), ('Azerbaijão'), ('Bahamas'),
+('Bahrein'), ('Bangladesh'), ('Barbados'), ('Bélgica'), ('Belize'),
+('Benin'), ('Bielorrússia'), ('Bolívia'), ('Bósnia e Herzegovina'), ('Botsuana'),
+('Brasil'), ('Brunei'), ('Bulgária'), ('Burkina Faso'), ('Burundi'),
+('Cabo Verde'), ('Camarões'), ('Camboja'), ('Canadá'), ('Catar'),
+('Cazaquistão'), ('Chade'), ('Chile'), ('China'), ('Chipre'),
+('Colômbia'), ('Comores'), ('Coreia do Norte'), ('Coreia do Sul'), ('Costa do Marfim'),
+('Costa Rica'), ('Croácia'), ('Cuba'), ('Dinamarca'), ('Djibuti');
