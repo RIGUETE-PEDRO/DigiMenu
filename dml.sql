de0241f385a008db027b5a56639228a7499a368e
PEDRO HENRIQUE RIGUETE DE OLIVEIRA MARTINS <154231408+RIGUETE-PEDRO@users.noreply.github.com>
2025-10-16 23:57:08 -0300

    Insert new tasks and order statuses in dml.sql
    Added new tasks and order statuses to the database.

--- dml.sql
+++ dml.sql
@@ -3,6 +3,19 @@ INSERT INTO TipoUsuario(TipoUsuario) VALUES ('Admin');
 
 INSERT INTO Tarefas(Tarefa) VALUES ('Cadastrar Usuario');
 INSERT INTO Tarefas(Tarefa) VALUES ('login');
+insert into Tarefas (Tarefa) values ('Cadastro de Produto');
+insert into Tarefas (Tarefa) values ('Atualização de Produto');
+insert into Tarefas (Tarefa) values ('Delete de produto ');
+insert into Tarefas (Tarefa) values ('adicionar produto no carrinho');
+
+
+--status de pedido
+insert into status values ('pendente')
+insert into status values ('em preparo')
+insert into status values ('pronto')
+insert into status values ('saiu para entrega')
+insert into status values ('finalizado')
+
 
 
 --insert de paises 
