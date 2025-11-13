e79e36eca3bdb63e4501784d4971947a8240b2ec
PEDRO HENRIQUE RIGUETE DE OLIVEIRA MARTINS <154231408+RIGUETE-PEDRO@users.noreply.github.com>
2025-11-13 20:44:28 -0300

    subindo carrousel e styles

--- DigiMenu/Default.aspx.designer.cs
+++ DigiMenu/Default.aspx.designer.cs
@@ -105,15 +105,6 @@ namespace DigiMenu
         protected global::System.Web.UI.WebControls.Repeater rptCarousel;
 
         /// <summary>
-        /// Controle rbPreco.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RadioButton rbPreco;
-
-        /// <summary>
         /// Controle txtPrecoMin.
         /// </summary>
         /// <remarks>
